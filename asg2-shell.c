5333872de8312933fe30343008ce574631d6317c
Energy0124 <energystar.0124@gmail.com>
2016-10-12 11:47:54 +0800

    add check current directory

--- asg2-shell.c
+++ asg2-shell.c
@@ -8,26 +8,41 @@
 /* Header Declaration */
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+
+/*Global Variable*/
+char currentDirectory[1024]="";
+char *tokens[255];
+
 
 /* Function Declaration */
-int getUserInput(char* input);
-void tokenizeInput(char* input);
+int getUserInput(char *input);
+
+void tokenizeInput(char *input);
+
+void gofolder(char *pString[255]);
 
 /* Functions */
-int main(int argc,char* argv[])
-{
-  int isExit = 0;
-  do {
-    char rawInput[255];
-    isExit = getUserInput(rawInput);
-    if(isExit) break;
+int main(int argc, char *argv[]) {
+    int isExit = 0;
 
-    tokenizeInput(rawInput);
+    //init cwd
+    getcwd(currentDirectory, sizeof(currentDirectory));
+
+    do {
+        char rawInput[255];
+        isExit = getUserInput(rawInput);
+        if (isExit) break;
+
+        tokenizeInput(rawInput);
 
 
-
-  } while(isExit = 1);
-  return 0;
+    } while (isExit = 1);
+    return 0;
 }
 
 /*
@@ -36,23 +51,22 @@ int main(int argc,char* argv[])
   - Copy the cleansed input to parameter.
   - Return 1 if encountered EOF (Ctrl-D), 0 otherwise.
 */
-int getUserInput(char* input)
-{
-  char buf[255];
-  char *s = buf;
-  printf("[3150 Shell:%s]=> ","PATH");
-  if(fgets(buf,255,stdin) == NULL)
-  {
-    putchar('\n');
-    return 1;
-  }
-  // Remove \n
-  for(;*s != '\n';s++);
-  *s = '\0';
+int getUserInput(char *input) {
+    char buf[255];
+    char *s = buf;
+    printf("[3150 Shell:%s]=> ", currentDirectory);
+    if (fgets(buf, 255, stdin) == NULL) {
+        putchar('\n');
+        return 1;
+    }
+    // Remove \n
+    for (; *s != '\n'; s++);
+    *s = '\0';
 
-  strcpy(input,buf);
-  return 0;
+    strcpy(input, buf);
+    return 0;
 }
+
 /*
   tokenizeInput()
   - Tokenize the string stored in parameter, delimiter is space
@@ -60,16 +74,31 @@ int getUserInput(char* input)
   Please implement the remaining part.
 */
 
-void tokenizeInput(char* input)
-{
-  char buf[255];
-  strcpy(buf,input);
+void tokenizeInput(char *input) {
+    char buf[255];
+    strcpy(buf, input);
 
-  char* token = strtok(buf," ");
-  while(token != NULL)
-  {
-    printf("%s\n",token);
-    token = strtok(NULL," ");
-  }
-  return;
+    char *token = strtok(buf, " ");
+    int count=0;
+    while (token != NULL) {
+        tokens[count]= token;
+    //    printf("%s\n", tokens[count]);
+        token = strtok(NULL, " ");
+        count++;
+    }
+    int i;
+
+    if(strcmp(tokens[0],"gofolder")==0){
+        gofolder(tokens);
+    }else{
+
+    }
+
+    return;
+}
+
+void gofolder(char *args[255]) {
+//    if(args[1][0]=='\0'){
+//        printf("%d","gofolder: wrong number of arguments\n");
+//    }
 }
